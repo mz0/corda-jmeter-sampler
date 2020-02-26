1c15e7784bb7a0a65a7985e38661f0cf3b2273ab
Mark Zhitomirski <mz@exactpro.com>
2020-02-27 00:09:21 +0300

    more tidy-up & check Corda 4.2

--- src/main/kotlin/aa/HelloTest2.kt
+++ src/main/kotlin/aa/HelloTest2.kt
@@ -2,6 +2,8 @@ package aa
 
 import java.io.Serializable
 import net.corda.client.rpc.CordaRPCClient
+import net.corda.client.rpc.CordaRPCConnection
+import net.corda.core.messaging.CordaRPCOps
 import net.corda.core.utilities.NetworkHostAndPort
 import org.apache.jmeter.config.Argument
 import org.apache.jmeter.config.Arguments
@@ -14,11 +16,15 @@ import org.slf4j.LoggerFactory
 
 class HelloTest2:  JavaSamplerClient, Serializable, Interruptible {
     private lateinit var samTag: String
+    private lateinit var rpcOps: CordaRPCOps
+    private lateinit var node: String
+    private lateinit var client: CordaRPCClient
+    private lateinit var connection: CordaRPCConnection
     @Transient lateinit var myThread: Thread
 
     companion object {
         private const val serialVersionUID = 20191112L
-        private val LOG = LoggerFactory.getLogger("AA")
+        private val LOG = LoggerFactory.getLogger("nft")
         val host = Argument("host", "192.168.1.191")
         val port = Argument("port", "10001")
         val username = Argument("RPC username", "User1")
@@ -26,34 +32,35 @@ class HelloTest2:  JavaSamplerClient, Serializable, Interruptible {
         val rpcArgs = setOf(host, port, username, password)
 
         val other =  Argument("other party","O=AAVB, L=Bilbao, C=ES")
-        val amount = Argument("amount","1,000.99 USD")
-        val additionalArgs = setOf(other, amount)
+        val numStates = Argument("numberOfStatesPerTx", "1")
+        val changeStates = Argument("numberOfChangeStatesPerTx", "1")
+        val anon = Argument("anonymousIdentities", "false")
+
+        val additionalArgs = setOf(other, numStates, changeStates, anon)
     }
 
     override fun setupTest(context: JavaSamplerContext) {
-        samTag = context.getParameter(TestElement.NAME);
+        samTag = context.getParameter(TestElement.NAME)
+        node = "Corda node ${context.getParameter(host.name)}:${context.getIntParameter(port.name)}"
+        LOG.info("$node test setup start")
+        client = CordaRPCClient(NetworkHostAndPort(context.getParameter(host.name), context.getIntParameter(port.name)))
+        LOG.info("$node CordaRPCClient created")
     }
 
     override fun runTest(context: JavaSamplerContext): SampleResult {
+        connection = client.start(context.getParameter(username.name), context.getParameter(password.name))
+        LOG.info("$node CordaRPCClient connectton started")
+        rpcOps = connection.proxy
+        LOG.info("$node test setup done")
+
         val results = SampleResult();
         results.setSampleLabel(samTag);
-        val node = "Corda node ${context.getParameter(host.name)}:${context.getIntParameter(port.name)}"
-        LOG.info("$node test setup start")
-        // prepare test
-        val client = CordaRPCClient(NetworkHostAndPort(context.getParameter(host.name), context.getIntParameter(port.name)))
-        LOG.info("$node CordaRPCClient created")
-        val connection = client.start(context.getParameter(username.name), context.getParameter(password.name))
-        LOG.info("$node CordaRPCClient connectton started")
-        val rpcOps = connection.proxy
-        LOG.info("$node test setup done")
         results.setSamplerData("Test node: " + context.getParameter(host.name));
         try {
             results.sampleStart(); // Record sample start time.
             myThread = Thread.currentThread();
             // do test
             LOG.info("$node time was: ${rpcOps.currentNodeTime()}")
-            connection.notifyServerAndClose()
-            LOG.info("$node CordaRPCClient connectton closed.")
             results.setSuccessful(true);
         } catch (e: InterruptedException) {
             LOG.warn("$node test interrupted.");
@@ -81,7 +88,14 @@ class HelloTest2:  JavaSamplerClient, Serializable, Interruptible {
         }
     }
 
-    override fun teardownTest(context: JavaSamplerContext?) { }
+    override fun teardownTest(context: JavaSamplerContext?) {
+        try {
+            connection.notifyServerAndClose()
+            LOG.info("$node CordaRPCClient connectton closed.")
+        } catch (e: Exception) {
+            LOG.error("Error closing node connection: $e")
+        }
+    }
 
     override fun interrupt(): Boolean {
         val alive = ::myThread.isInitialized
